8c6c7a022e21b3fbb88b4fa84bcf3b7bab99f1bb
Marco Ceppi <mceppi@danya.com>
2010-08-09 21:16:19 -0400

    Basic templating file

---
+++ lib/template.php
@@ -0,0 +1,36 @@
+<?php
+
+if( !$simple )
+{
+	?>
+<html>
+<head>
+<title>Virtual Review<?php echo (isset($page_title)) ? " : " . $page_title : ""; ?></title>
+<link rel="stylesheet" type="text/css" href="<?php getWebRoot(true); ?>assets/css/main.css">
+</head>
+<body>
+<div id="head"></div>
+<div id="container">
+	<div id="main">
+		<?php if( isset($msg) )     { print "<div class=\"alert\">$msg</div>"; } ?>
+		<?php if( isset($error) )   { print "<div class=\"error\">$error</div>"; } ?>
+		<?php if( isset($success) ) { print "<div class=\"success\">$success</div>"; } ?>
+		<p></p>
+		<?php include($view_file); ?>
+	</div>
+</div>
+</body>
+</html>
+	<?php
+}
+else
+{
+	?>
+		<?php if( isset($msg) )     { print "<div class=\"alert\">$msg</div>"; } ?>
+		<?php if( isset($error) )   { print "<div class=\"error\">$error</div>"; } ?>
+		<?php if( isset($success) ) { print "<div class=\"success\">$success</div>"; } ?>
+		<p></p>
+		<?php include($view_file); ?>
+	<?php
+}
+?>
