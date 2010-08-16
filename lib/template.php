da934fc7d6a637bd2616f934748e6ee0cf65e9b9
Marco Ceppi <marco.ceppi@seacrow.org>
2010-08-16 15:55:36 -0400

    This was not needed and messed with the Ajax

--- lib/template.php
+++ lib/template.php
@@ -32,7 +32,6 @@ else
 		<?php if( isset($msg) )     { print "<div class=\"alert\">$msg</div>"; } ?>
 		<?php if( isset($error) )   { print "<div class=\"error\">$error</div>"; } ?>
 		<?php if( isset($success) ) { print "<div class=\"success\">$success</div>"; } ?>
-		<p></p>
 		<?php include($view_file); ?>
 	<?php
 }
