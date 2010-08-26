15e0ca66adff153c158efa6cce26166c4cbb133b
Marco Ceppi <marco.ceppi@seacrow.org>
2010-08-26 12:18:32 -0400

    Updated DB scheme for multiple users

--- contrib/guardian.sql
+++ contrib/guardian.sql
@@ -47,6 +47,34 @@ INSERT INTO `sites` VALUES (1,1,'My Awesome Site','/home/marco/virtual-review/')
 UNLOCK TABLES;
 
 --
+-- Table structure for table `users_sites`
+--
+
+DROP TABLE IF EXISTS `users_sites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_sites` (
+  `user_id` int(11) NOT NULL,
+  `site_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`site_id`),
+  KEY `fk_users_sites_1` (`user_id`),
+  KEY `fk_users_sites_2` (`site_id`),
+  CONSTRAINT `fk_users_sites_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_users_sites_2` FOREIGN KEY (`site_id`) REFERENCES `sites` (`site_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_sites`
+--
+
+LOCK TABLES `users_sites` WRITE;
+/*!40000 ALTER TABLE `users_sites` DISABLE KEYS */;
+INSERT INTO `users_sites` VALUES (1,1),(1,2);
+/*!40000 ALTER TABLE `users_sites` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -83,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-08-12 20:08:08
+-- Dump completed on 2010-08-26 12:17:00
