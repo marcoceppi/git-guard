25930a641b60f08f8477f23c9e5bf8bf79471ac4
Marco Ceppi <marco.ceppi@seacrow.org>
2010-09-01 18:32:03 -0400

    To hell with Caching!

--- lib/template.php
+++ lib/template.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Expires: Mon, 20 Dec 1998 01:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-cache, must-revalidate");
+header("Pragma: no-cache");
+
 if( !is_simple() && !$is_simple )
 {
 	?>
